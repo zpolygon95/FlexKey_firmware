b6a864e6d65b1a956b7ec163c5f7b096e19855d9
Zachary Polikarpus <zpolygon95@gmail.com>
2019-09-10 11:24:31 -0400

    Added USB application code

--- FlexKey_USB/FlexKey_USB.ino
+++ FlexKey_USB/FlexKey_USB.ino
@@ -1,12 +1,13 @@
+// Based on the example code in the Sparkfun ProMicro hookup guide
+// https://learn.sparkfun.com/tutorials/pro-micro--fio-v3-hookup-guide
+// Original licence text below:
+
 /* HID KeyBoard Example
 by: Jim Lindblom
 date: 1/12/2012
 license: MIT License - Feel free to use this code for any purpose.
 No restrictions. Just keep this license if you go on to use this
 code in your future endeavors! Reuse and share. 
-
-This is very simplistic code that allows you to send a 'z' with
-a momentary pushbutton. 
 */
 
 #include <Keyboard.h>
@@ -18,13 +19,17 @@ int count = 0;
 #define THRESHOLD 500
 #define ROLLOVER 1000
 
-char *toType = "Hello, world!\n";
+char hidBuf[256];
+int hidIdx = 0;
+char cur;
 
 void setup()
 {
   pinMode(RXLED, OUTPUT);  // Set RX LED as an output
   pinMode(buttonPin, INPUT);  // Set the button as an input
   digitalWrite(buttonPin, HIGH);  // Pull the button high
+  Serial.begin(9600);
+  Serial1.begin(9600);
 }
 
 void typeString(char *str, int len) {
@@ -35,20 +40,50 @@ void loop()
 {
   delay(1);
   count++;
-  if (count == 1) TXLED0;
-  else if (count == THRESHOLD) TXLED1;
+  if (count == 1) {
+    TXLED0;
+//    Serial1.print(".");
+  }
+  else if (count == THRESHOLD) {
+    TXLED1;
+  }
   else if (count >= ROLLOVER) count = 0;
 
-  if (digitalRead(buttonPin) == 0 && debounce == 0)  debounce = 1;
-  if (debounce == 1)
-  {
-    digitalWrite(RXLED, LOW);   // set the RX LED ON
-    typeString(toType, strlen(toType));
-    debounce++;
-  }
-  else if (debounce > 0 && debounce < THRESHOLD) debounce++;
-  else if (debounce >= THRESHOLD) {
-    debounce = 0;
-    digitalWrite(RXLED, HIGH);   // set the RX LED OFF    
+  while (Serial1.available() > 0) {
+    cur = (char) Serial1.read();
+    Serial.write(cur);
+    if (hidIdx == 0 && cur == '\\') hidBuf[hidIdx++] = cur;
+    else if (hidIdx > 0 && hidBuf[hidIdx - 1] == '\\') {
+      // Define escape sequences here
+      if (cur == '0') {
+        // "\0" - End of packet
+        typeString(&hidBuf[2], hidIdx - 3);
+        hidIdx = 0;
+        Serial1.print("OK");
+      } else if (cur == '1') {
+        // "\1" - Start of packet
+        hidBuf[hidIdx++] = cur;
+      } else if (cur == '\\') {
+        // "\\" - Literal backslash
+        // There's already a backslash in the buffer at the correct point. This is a NOP
+      } else if (cur == 't') {
+        // "\t" - Tab
+        hidBuf[hidIdx - 1] = '\t';
+      } else if (cur == 'n') {
+        // "\n" - New line
+        hidBuf[hidIdx - 1] = '\n';
+      } else {
+        // Unknown Control Character
+        hidIdx = 0;
+        Serial1.print(cur);
+        Serial1.print("?");
+      }
+    } else if (hidIdx > 0) {
+      hidBuf[hidIdx++] = cur;
+    } else {
+      // Unknown State
+      hidIdx = 0;
+      Serial1.print("?");
+    }
   }
 }
