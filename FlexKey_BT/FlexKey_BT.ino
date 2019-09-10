59abcc5d123c1203d1e02aae8383c4740822686e
Zachary Polikarpus <zpolygon95@gmail.com>
2019-09-10 11:23:09 -0400

    Add Bluetooth application code

--- FlexKey_BT/FlexKey_BT.ino
+++ FlexKey_BT/FlexKey_BT.ino
@@ -1,72 +1,35 @@
+// Modified from Sandeep Mistry's arduino-BLEPeripheral project
+// https://github.com/sandeepmistry/arduino-BLEPeripheral
+
+/*
+ * Serial Port over BLE
+ * Create UART service compatible with Nordic's *nRF Toolbox* and Adafruit's *Bluefruit LE* iOS/Android apps.
+ */
+
+
 // Import libraries (BLEPeripheral depends on SPI)
 #include <SPI.h>
 #include <BLEPeripheral.h>
+#include "BLESerial.h"
 
-//////////////
-// Hardware //
-//////////////
-#define LED_PIN    7 // LED on pin 7
-#define LED_ACTIVE LOW // Pin 7 LED is active low
-#define LED_DEFAULT LOW
+//custom boards may override default pin definitions with BLESerial(PIN_REQ, PIN_RDY, PIN_RST)
+BLESerial bleSerial;
 
-#define OUT_PIN    8 // OUT on pin 8
-#define OUT_ACTIVE LOW // Pin 8 OUT is active low
 
-///////////////////////
-// BLE Advertisments //
-///////////////////////
-const char * localName = "FlexKey";
-BLEPeripheral blePeriph;
-BLEService bleServ("1207");
-BLECharCharacteristic ledChar("1207", BLERead | BLEWrite);
+void setup() {
+  // custom services and characteristics can be added as well
+  bleSerial.setLocalName("FlexKey");
 
-void setup()
-{
-  Serial.begin(115200); // Set up serial at 115200 baud
-
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, !LED_ACTIVE);
-  pinMode(OUT_PIN, OUTPUT);
-  digitalWrite(OUT_PIN, !OUT_ACTIVE);
-
-  setupBLE();
+  Serial.begin(9600);
+  bleSerial.begin();
 }
 
-void loop()
-{
-  blePeriph.poll();
+void loop() {
+  bleSerial.poll();
 
-  if (ledChar.written())
-  {
-    int ledState = ledChar.value();
-    if (ledState) {
-      digitalWrite(LED_PIN, LED_ACTIVE);
-      digitalWrite(OUT_PIN, OUT_ACTIVE);
-    }
-    else {
-      digitalWrite(LED_PIN, !LED_ACTIVE);
-      digitalWrite(OUT_PIN, !OUT_ACTIVE);
-    }
+if (bleSerial && Serial) {
+    int byte;
+    while ((byte = bleSerial.read()) > 0) Serial.write((char)byte);
+    while ((byte = Serial.read()) > 0) bleSerial.write((char)byte);
   }
-}
-
-void setupBLE()
-{
-  // Advertise name and service:
-  blePeriph.setDeviceName(localName);
-  blePeriph.setLocalName(localName);
-  blePeriph.setAdvertisedServiceUuid(bleServ.uuid());
-
-  // Add service
-  blePeriph.addAttribute(bleServ);
-
-  // Add characteristic
-  blePeriph.addAttribute(ledChar);
-
-  // Now that device6, service, characteristic are set up,
-  // initialize BLE:
-  blePeriph.begin();
-
-  // Set led characteristic to default value:
-  ledChar.setValue(!LED_ACTIVE);
 }
